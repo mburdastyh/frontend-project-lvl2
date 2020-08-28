d90eb6d290c0c0b63602efcb533d17281e218aee
mburdastyh <mburdastyh@gmail.com>
2020-08-28 23:25:40 +0700

    Step2: add arguments and format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,18 @@
 import program from 'commander';
 
-program.version('0.0.1');
-program.description('Compares two configuration files and shows a difference.');
-
-program.helpOption('-h, --help', 'output usage information');
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log('first file is', filepath1);
+    console.log('second file is', filepath2);
+  });
 
 program.parse(process.argv);
+
+if (program.format) {
+  console.log('format is', program.format);
+}
